6ec5110780a71fff7dca4a948a299197a93a58cb
Vazgen Vardevanyan <vazgentigranich@gmail.com>
2019-09-14 16:11:04 +0400

    send axios request

--- task-test/src/components/SlotRequests.vue
+++ task-test/src/components/SlotRequests.vue
@@ -1,56 +1,63 @@
 <template>
-	<div>
-		<h3 class="py-3">Requests Count: {{countRequests}}</h3>		
-		<b-table striped hover :items="results"></b-table>
-	</div>
+    <div>
+        <h3 class="py-3">Requests Count: {{countRequests}}</h3>        
+        <b-table striped hover :items="results"></b-table>
+    </div>
 </template>
 
 <script>
-	import _ from 'lodash'
+    import _ from 'lodash'
+    import axios from 'axios'
 
-	export default {
-		data() {
-			const countRequests = window.localStorage.getItem('requestsCount') || 0;
-			return {
-				countRequests,
-				results: [
-				]
-			}
-		},
+    export default {
+        data() {
+            const countRequests = window.localStorage.getItem('requestsCount') || 0;
+            return {
+                countRequests,
+                results: [
+                ]
+            }
+        },
 
-		mounted() {
-			this.interval = setInterval(() => {
-				const sum = _.random(10, 100);
-				const randStatusIndex = _.random(0, 1)
-				const statuses = [
-					{
-						label: 'WIN',
-						rowVariant: 'success'
-					},
-					{
-						label: 'LOST',
-						rowVariant: 'danger'
-					},
-				];
+        mounted() {
+            this.interval = setInterval(() => {
+                return;
+                const sum = _.random(100, 200);
+                const randStatusIndex = _.random(0, 1)
+                const user_id = _.random(4, 6);
 
-				const { label, rowVariant } = statuses[randStatusIndex];
+                axios.post('http://localhost:8000/api/spin',{
+                    user_id,
+                    sum,
+                    currency_id: _.random(1, 2)
+                }).then(response => {
+                    const { status, _status, user_id } = response.data
+                    const rowVariant = _status === 'WIN' ? 'success': 'danger'
 
-				this.results.splice(0, 0, {
-					status: label,
-					bet: sum,
-					_rowVariant: rowVariant
-				});
+                    if (status === 'success') {
+                        this.results.splice(0, 0, {
+                            user_id,
+                            status: _status,
+                            bet: sum,
+                            _rowVariant: rowVariant
+                        })
+                    } else {
+                        console.log(response.data.reason);
+                    }
+                }).catch(result => {
+                    console.log(result);
+                })
 
-				if (this.results.length > 10) {
-					this.results.pop();
-				}
-				this.countRequests++;
-				window.localStorage.setItem('requestsCount', this.countRequests);
-			}, 1000);
-		},
+                if (this.results.length > 10) {
+                    this.results.pop();
+                }
+                this.countRequests++;
+                window.localStorage.setItem('requestsCount', this.countRequests);
+            }, 15);
+        },
 
-		beforeDestroy() {
-			clearInterval(this.interval);
-		}
-	}
+        beforeDestroy() {
+            clearInterval(this.interval);
+        }
+    }
 </script>
